d1e0e926accffa3a4a322195548f3a41204d72a8
danestrin <danestrin4@gmail.com>
2018-02-09 21:27:16 -0800

    initial commit

---
+++ jamrun/android/src/com/bacc/jamrun/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bacc.jamrun;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bacc.jamrun.JamRun;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JamRun(), config);
+	}
+}
---
+++ jamrun/desktop/src/com/bacc/jamrun/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.bacc.jamrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bacc.jamrun.JamRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JamRun(), config);
+		config.title = "BC Game Jam 2018";
+		config.width = 640;
+		config.height = 320;
+	}
+}
